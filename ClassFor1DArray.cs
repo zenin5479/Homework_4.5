fab9394b8cdd20ef9f9a7c94a8173a3e60d12a4c
zenin5479 <zenin5479@gmail.com>
2025-06-06 08:26:18 +0300

    Update 3.2 Обновлен метод

--- ClassFor1DArray.cs
+++ ClassFor1DArray.cs
@@ -137,10 +137,10 @@ namespace Homework_4._5
          while (i < inputArray.Length)
          {
             // Сравниваем значения double используя метод Equals(Double)
-            //if (inputArray[i].Equals(flag))
-            //{
-            //   count++;
-            //}
+            if (inputArray[i].Equals(flag))
+            {
+               count++;
+            }
 
             // Сравниваем значения double используя метод CompareTo(Double) 
             if (inputArray[i].CompareTo(flag) == 0)
@@ -149,10 +149,10 @@ namespace Homework_4._5
             }
 
             // Сравниваем значения double используя оператор равенства ==
-            if (inputArray[i] == 0)
-            {
-               count++;
-            }
+            //if (inputArray[i] == 0)
+            //{
+            //   count++;
+            //}
 
             i++;
          }
