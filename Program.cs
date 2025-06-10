995b6130f3b9b364433c41181cc0020f80cbc0c8
zenin5479 <zenin5479@gmail.com>
2025-06-10 11:43:39 +0300

    Update 9.5 Обновлен метод

--- Program.cs
+++ Program.cs
@@ -102,8 +102,6 @@ namespace Homework_4._5
             }
          }
 
-
-
          //if (fla == false)
          //{
          //   zamenanol(a, na); +
@@ -118,6 +116,23 @@ namespace Homework_4._5
          //}
 
          Console.ReadKey();
+      }
+
+      public static bool IsTextFileEmpty(string fileName)
+      {
+         var info = new FileInfo(fileName);
+         if (info.Length == 0)
+         {
+            return true;
+         }
+
+         // only if your use case can involve files with 1 or a few bytes of content.
+         if (info.Length < 6)
+         {
+            var content = File.ReadAllText(fileName);
+            return content.Length == 0;
+         }
+         return false;
       }
    }
 }
