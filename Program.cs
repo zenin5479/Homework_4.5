713568fc881d0d900f8a0917f00ab3e5d831bdec
zenin5479 <zenin5479@gmail.com>
2025-06-12 15:20:03 +0300

    Update 14.1 Обновлен метод

--- Program.cs
+++ Program.cs
@@ -72,9 +72,6 @@ namespace Homework_4._5
             //FileStream fileStream = new FileStream(filePathFour, FileMode.Open);
             //fileStream.SetLength(0);
             //fileStream.Close();
-
-
-
          }
 
          double[] arrayDoubleOne = ClassFor1DArray.VvodArray(filePathOne, nameArrayOne);
